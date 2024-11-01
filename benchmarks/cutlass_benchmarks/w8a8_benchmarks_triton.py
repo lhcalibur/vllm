e005e376b60e29b5cb3cc181f6c2a9114b326d6c
lhcalibur <liuhan@hotmail.com>
2024-11-01 08:02:26 +0000

    add triron custom ops

--- benchmarks/cutlass_benchmarks/w8a8_benchmarks_triton.py
+++ benchmarks/cutlass_benchmarks/w8a8_benchmarks_triton.py
@@ -12,11 +12,12 @@ from weight_shapes import WEIGHT_SHAPES
 
 from vllm import _custom_ops as cuda_ops
 from vllm import _triton_ops as triton_ops
-from vllm.utils import (STR_DTYPE_TO_TORCH_DTYPE, FlexibleArgumentParser)
+from vllm.utils import FlexibleArgumentParser
 
 DEFAULT_MODELS = list(WEIGHT_SHAPES.keys())
 DEFAULT_BATCH_SIZES = [1, 16, 32, 64, 128, 256, 512]
 DEFAULT_TP_SIZES = [1]
+
 
 # helpers
 def to_fp8(tensor: torch.Tensor) -> torch.Tensor:
@@ -24,8 +25,10 @@ def to_fp8(tensor: torch.Tensor) -> torch.Tensor:
     return torch.round(tensor.clamp(
         min=finfo.min, max=finfo.max)).to(dtype=torch.float8_e4m3fn)
 
+
 def to_int8(tensor: torch.Tensor) -> torch.Tensor:
     return torch.round(tensor.clamp(min=-128, max=127)).to(dtype=torch.int8)
+
 
 def make_rand_tensors(dtype: torch.dtype, m: int, n: int,
                       k: int) -> Tuple[torch.Tensor, torch.Tensor]:
@@ -39,42 +42,50 @@ def make_rand_tensors(dtype: torch.dtype, m: int, n: int,
 
     raise ValueError("unsupported dtype")
 
+
 # bench
-def bench_int8(dtype: torch.dtype, MKNs: Iterable[Tuple[int, int, int]], tp_size: Optional[int] = None) -> List[triton.testing.Benchmark]:
+def bench_int8(
+        dtype: torch.dtype,
+        MKNs: Iterable[Tuple[int, int, int]],
+        tp_size: Optional[int] = None) -> List[triton.testing.Benchmark]:
     assert dtype == torch.int8
     configs = []
     for with_bias in [True, False]:
         for with_azp_per_tensor in [True, False]:
             for with_azp_per_token in [True, False]:
-                plot_name = f"int8_gemm-{dtype}-bias-{with_bias}-azp-per-tensor-{with_azp_per_tensor}-azp-per-token-{with_azp_per_token}"
+                plot_name = (f"int8_gemm-{dtype}-bias-{with_bias}-"
+                             f"azp-per-tensor-{with_azp_per_tensor}-"
+                             f"azp-per-token-{with_azp_per_token}")
                 if tp_size is not None:
                     plot_name += f"-tp-{tp_size}"
-                configs.append(triton.testing.Benchmark(
-                    x_names=["m", "k", "n"],
-                    x_vals=MKNs,
-                    line_arg="version",
-                    line_vals=["cuda", "triton"],
-                    line_names=["CUDA", "Triton"],
-                    styles=[("blue", "-"), ("red", "-")],
-                    ylabel="Runtime (ms)",
-                    plot_name=plot_name,
-                    args={
-                        "dtype": dtype,
-                        "with_bias": with_bias,
-                        "with_azp_per_tensor": with_azp_per_tensor,
-                        "with_azp_per_token": with_azp_per_token,
-                    },
-                )
-            )
+                configs.append(
+                    triton.testing.Benchmark(
+                        x_names=["m", "k", "n"],
+                        x_vals=MKNs,
+                        line_arg="version",
+                        line_vals=["cuda", "triton"],
+                        line_names=["CUDA", "Triton"],
+                        styles=[("blue", "-"), ("red", "-")],
+                        ylabel="Runtime (ms)",
+                        plot_name=plot_name,
+                        args={
+                            "dtype": dtype,
+                            "with_bias": with_bias,
+                            "with_azp_per_tensor": with_azp_per_tensor,
+                            "with_azp_per_token": with_azp_per_token,
+                        },
+                    ))
 
-    def _bench(dtype: torch.dtype, m: int, k: int, n: int, with_bias: bool, with_azp_per_tensor: bool, with_azp_per_token: bool, version: str):
+    def _bench(dtype: torch.dtype, m: int, k: int, n: int, with_bias: bool,
+               with_azp_per_tensor: bool, with_azp_per_token: bool,
+               version: str):
         a, b = make_rand_tensors(dtype, m, n, k)
         scale_a = torch.tensor(1.0, device="cuda", dtype=torch.float32)
         scale_b = torch.tensor(1.0, device="cuda", dtype=torch.float32)
         bias = torch.zeros((n, ), device="cuda", dtype=torch.bfloat16)
         azp = torch.zeros((m, ), device="cuda", dtype=torch.int32)
         azp_adj = torch.zeros((n, ), device="cuda", dtype=torch.int32)
-        
+
         args = [a, b, scale_a, scale_b, torch.bfloat16]
         kwargs = {}
         if with_azp_per_token:
@@ -96,16 +107,22 @@ def bench_int8(dtype: torch.dtype, MKNs: Iterable[Tuple[int, int, int]], tp_size
                 ops = triton_ops.triton_scaled_mm_azp
             else:
                 ops = triton_ops.triton_scaled_mm
+
         def bench_fn():
             ops(*args, **kwargs)
-        
-        ms, min_ms, max_ms = triton.testing.do_bench(bench_fn, quantiles=[0.5, 0.2, 0.8])
+
+        ms, min_ms, max_ms = triton.testing.do_bench(bench_fn,
+                                                     quantiles=[0.5, 0.2, 0.8])
         return ms, max_ms, min_ms
 
     triton.testing.perf_report(configs)(_bench).run()
 
+
 # TODO support fp8
-def bench(dtype: torch.dtype, MKNs: Iterable[Tuple[int, int, int]]) -> List[triton.testing.Benchmark]:
+def bench(
+        dtype: torch.dtype,
+        MKNs: Iterable[Tuple[int, int,
+                             int]]) -> List[triton.testing.Benchmark]:
     if dtype == torch.int8:
         return bench_int8(dtype, MKNs)
     if dtype == torch.float8_e4m3fn:
@@ -113,10 +130,15 @@ def bench(dtype: torch.dtype, MKNs: Iterable[Tuple[int, int, int]]) -> List[trit
         raise NotImplementedError("FP8 is not supported yet")
     raise ValueError("unsupported type")
 
+
 def run(dtype: torch.dtype, MKNs: Iterable[Tuple[int, int, int]]):
     bench(dtype, MKNs)
 
-def make_output(data, MKNs: Iterable[Tuple[int, int, int]], base_description: str, timestamp=None):
+
+def make_output(data,
+                MKNs: Iterable[Tuple[int, int, int]],
+                base_description: str,
+                timestamp=None):
     print(f"== All Results {base_description} ====")
     for result in data:
         print(result)
@@ -125,10 +147,13 @@ def make_output(data, MKNs: Iterable[Tuple[int, int, int]], base_description: st
     with open(f"{base_description}-{timestamp}.pkl", "wb") as f:
         pkl.dump(data, f)
 
+
 def run_square_bench(args):
-    dim_sizes = list(range(args.dim_start, args.dim_end + 1, args.dim_increment))
+    dim_sizes = list(
+        range(args.dim_start, args.dim_end + 1, args.dim_increment))
     MKNs = list(zip(dim_sizes, dim_sizes, dim_sizes))
     run(args.dtype, MKNs)
+
 
 def run_range_bench(args):
     dim_sizes = list(range(args.dim_start, args.dim_end, args.dim_increment))
@@ -138,6 +163,7 @@ def run_range_bench(args):
     Ns = [args.n_constant] * n if args.n_constant is not None else dim_sizes
     MKNs = list(zip(Ms, Ks, Ns))
     run(args.dtype, MKNs)
+
 
 def run_model_bench(args):
     print("Benchmarking models:")
@@ -162,7 +188,9 @@ def run_model_bench(args):
 
         run(args.dtype, MKNs)
 
+
 if __name__ == '__main__':
+
     def to_torch_dtype(dt):
         if dt == "int8":
             return torch.int8
@@ -175,16 +203,23 @@ if __name__ == '__main__':
 Benchmark Cutlass GEMM.
 
     To run square GEMMs:
-        python3 ./benchmarks/cutlass_benchmarks/w8a8_benchmarks.py --dtype fp8 square_bench --dim-start 128 --dim-end 512 --dim-increment 64
+        python3 ./benchmarks/cutlass_benchmarks/w8a8_benchmarks.py \\
+            --dtype fp8 square_bench --dim-start 128 \\
+            --dim-end 512 --dim-increment 64
     
     To run constant N and K and sweep M:
-        python3 ./benchmarks/cutlass_benchmarks/w8a8_benchmarks.py --dtype fp8 range_bench --dim-start 128 --dim-end 512 --dim-increment 64 --n-constant 16384 --k-constant 16384
+        python3 ./benchmarks/cutlass_benchmarks/w8a8_benchmarks.py \\
+            --dtype fp8 range_bench --dim-start 128 --dim-end 512 \\
+            --dim-increment 64 --n-constant 16384 --k-constant 16384
     
     To run dimensions from a model:
-        python3 ./benchmarks/cutlass_benchmarks/w8a8_benchmarks.py --dtype fp8 model_bench --models meta-llama/Llama-2-7b-hf --batch-sizes 16 --tp-sizes 1
+        python3 ./benchmarks/cutlass_benchmarks/w8a8_benchmarks.py \\
+            --dtype fp8 model_bench --models meta-llama/Llama-2-7b-hf \\
+            --batch-sizes 16 --tp-sizes 1
     
     Output:
-        - a .pkl file, that is a list of raw torch.benchmark.utils.Measurements for the pytorch and cutlass implementations for the various GEMMs.
+        - a .pkl file containing raw torch.benchmark.utils.Measurements \\
+          for the pytorch and cutlass implementations for the various GEMMs.
             """,
         formatter_class=argparse.RawTextHelpFormatter)
 
