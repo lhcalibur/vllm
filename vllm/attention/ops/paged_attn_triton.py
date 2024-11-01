5b0d257803c3a2397bcaa2feeb502c79ad00a727
lhcalibur <liuhan@hotmail.com>
2024-11-01 08:40:49 +0000

    add triron custom ops

--- vllm/attention/ops/paged_attn_triton.py
+++ vllm/attention/ops/paged_attn_triton.py
@@ -1,6 +1,7 @@
-# Modified from BAAI FlagAttention code
 """
-Copyright 2023 BAAI
+Copyright 2023 The vLLM team.
+Copyright 2023 The BAAI team.
+Copyright 2024 The SiOrigin Inc. team. All rights reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,8 +14,6 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-
-# TODO add SiOrigin License
 """
 
 from dataclasses import dataclass
